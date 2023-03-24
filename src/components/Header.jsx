70d14dbc60c0780a33a028fa58722ba1585651fc
dan pham <c0ngdan321@gmail.com>
2023-03-24 15:37:34 +0700

    refactor routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import logo from "../assets/img/logo_xoa_phong.png";
-import { useLocation, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { SelectBox } from "./common/SelectBox";
 import { SelectBoxItem } from "./common/SelectBoxItem";
 import * as appAction from '../modules/app'
@@ -27,31 +27,23 @@ export const Header = () => {
   }, [location.search, dispatch]);
 
   return (
-    <header className="header h-16 ring ring-slate-100 flex justify-between items-center px-4">
-      <div className="flex items-center">
-        <img alt="" src={logo} width={48} height={48} />
-        <h2 className="pl-2 font-bold text-xl uppercase">CCAMS BẮC HẢI</h2>
-      </div>
-      <div className="flex gap-2">
-        {/* <button
-          className={location.pathname === '/' ? 'btn-primary--contained' : 'btn-primary--outlined'}
-          onClick={() => navigate('/')}>
-          Report
-        </button>
-        <button
-          className={location.pathname === '/search' ? 'btn-primary--contained' : 'btn-primary--outlined'}
-          onClick={() => navigate('/search')}
-        >
-          Search
-        </button> */}
-        <div className="w-36">
-          <SelectBox name="Menu" value={activeUnit} nonSelected>
-            <SelectBoxItem name='Tra cứu đi lễ' value='' changeSelection={changeUnit} />
-            <SelectBoxItem name='Tìm kiếm' value='search' changeSelection={changeUnit} />
-            {isNganhAu && <SelectBoxItem name='Cấp lại thẻ' value='request-card' changeSelection={changeUnit} />}
-          </SelectBox>
+    <>
+      <header className="header h-16 ring ring-slate-100 flex justify-between items-center px-4">
+        <div className="flex items-center">
+          <img alt="" src={logo} width={48} height={48} />
+          <h2 className="pl-2 font-bold text-xl uppercase">CCAMS BẮC HẢI</h2>
         </div>
-      </div>
-    </header>
+        <div className="flex gap-2">
+          <div className="w-36">
+            <SelectBox name="Menu" value={activeUnit} nonSelected>
+              <SelectBoxItem name='Tra cứu đi lễ' value='' changeSelection={changeUnit} />
+              <SelectBoxItem name='Tìm kiếm' value='search' changeSelection={changeUnit} />
+              {isNganhAu && <SelectBoxItem name='Cấp lại thẻ' value='request-card' changeSelection={changeUnit} />}
+            </SelectBox>
+          </div>
+        </div>
+      </header>
+      <Outlet />
+    </>
   );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Header } from "../components/Header";
+// import { TestPage } from "../views/ListSMSPage";
 import { configRoutes } from "./configRoutes";
 
 const RoutesMainLayout = () => {
   return (
     <Routes>
-      {configRoutes.map((route) => {
-        return (
-          <Route
-            key={route.name}
-            exact
-            path={route.path}
-            element={route.component}
-            pathname={route.name}
-          />
-        );
-      })}
+      {/* <Route exact element={<TestPage />} path='/test' pathname='test' /> */}
+      <Route element={<Header />}>
+        {configRoutes.map((route) => {
+          return (
+            <Route
+              key={route.name}
+              exact
+              path={route.path}
+              element={route.component}
+              pathname={route.name}
+            />
+          );
+        })}
+      </Route>
     </Routes>
   );
 };
