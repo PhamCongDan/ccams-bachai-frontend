8bba0bdb9e21144893581cbe9e91f21fcfa4b3fe
dan pham <c0ngdan321@gmail.com>
2024-02-06 14:40:28 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        primary: 'blue-500',
+      },
       fontFamily: {
-        'serif': ['Times New Roman']
+        serif: ['Times New Roman'],
       },
       width: {
         sm: '300px',
-        md: '1200px'
+        md: '1200px',
       },
       height: {
         sm: '200px',
@@ -19,4 +20,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
